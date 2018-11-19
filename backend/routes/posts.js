4f20ea1b3bf5b436c30e790b2930050295a34af5
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2018-11-19 11:34:41 -0500

    re-factored backend post route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Model = require("../models/postModel");
-const Post = require("../models/registerModel");
+const Register = require("../models/registerModel");
 
 
 router.post("", (req, res) => {
-  Post.findOneAndUpdate({username: req.body.user},
+
+  Register.findOneAndUpdate({username: req.body.user.username},
   {$push: {picks: req.body.golfer}},
   function (error, success) {
         if (error) {
-            console.log(error);
+            console.log("ERRRORRRR");
         } else {
-            console.log(success);
+            console.log("Succcsersrse");
         }
     });
 })
 
-
-router.post("", (req, res) => {
-    
-    Post.findOne({username: req.body.user}, function (err, Post) {
-        console.log(req.body.user)
-        if (err) return handleError(err);
-      
-        Post.set({ picks: req.body.golfer});
-        Post.save(function (err, updatedPost) {
-          if (err) return handleError(err);
-          res.send(updatedPost);
-        });
-      });
-})
 
 module.exports = router;
